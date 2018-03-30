c3145be9134e7e12b8472a47bb590af32f62d07b
Ruchitesh Malukani <Ruchitesh22@gmail.com>
2018-03-30 18:11:55 +0530

    added operator overloading
    overloaded operator +

--- mypt.py
+++ mypt.py
@@ -4,6 +4,11 @@ class point:
         self.y=y
     def __str__(self):
         return (str(self.x) + '+' + str(self.y) + 'j' )
+	def __add__(self,other):
+		x=self.x+other.x
+		y=self.y+other.y
+		return point(x,y)
 
 p=point(2,3)
-print(p)
+q=point(4,5)
+print(p+q)
